4a5f7aeff1aee7678ad7dbf0eca173ebeba70a01
Colin Eyster <cjegy9@mst.edu>
2017-10-18 21:19:50 -0500

    Adding app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import randomNumber from 'random-number';
 
 import './App.css';
 import Display from './display';
+import Header from './Header';
+
+import {Button} from 'semantic-ui-react'
 
 class App extends Component {
   constructor(props) {
@@ -18,24 +21,42 @@ class App extends Component {
     axios({
       method: 'get',
       url: 'http://www.clashapi.xyz/api/random-deck',
-    })
-      .then(res => {
-        console.log(res);
+    }).then(res => {
         this.setState({cardArray: res.data});
-      })
+    })
+  }
+
+  newDeck() {
+    axios({
+      method: 'get',
+      url: 'http://www.clashapi.xyz/api/random-deck',
+    }).then(res => {
+      this.setState({cardArray: res.data});
+    })
   }
 
   render() {
     const { cardArray } = this.state;
     return (
       <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div style={{marginBottom: 16}}>
+          <Header/>
+        </div>
+        <div>
+          <Button
+            color='blue'
+            onClick={() => this.newDeck()}>
+            Generate New Deck
+          </Button>
+        </div>
         {typeof cardArray !== 'undefined' &&
-          <div>
+          <div style={{marginTop: 16}}>
             <Display
               cardArray={cardArray}
             />
           </div>
         }
+
       </div>
     );
   }
